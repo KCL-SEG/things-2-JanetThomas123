9310c2482d988f03a2e5e84f8904262c2bf19121
JanetThomas123 <115407383+JanetThomas123@users.noreply.github.com>
2022-12-09 00:11:42 +0000

    Update forms.py

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your forms here.
+class ThingForm(forms.Form):
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(50)]
+    )
+
+    class Meta:
+        exclude = ['created_at']
